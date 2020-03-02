c70758a7c8e2732adba0fea58f213840ee80596e
izabellacruz11 <60441007+izabellacruz11@users.noreply.github.com>
2020-03-02 12:38:16 -0500

    Add files via upload

---
+++ poweroftwo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Student Izabella Cruz
+ * created: 3/2/20
+ *
+ */
+import java.util.Scanner;
+public class poweroftwo {
+	public static void main(String[] args) {
+	Scanner kb = new Scanner(System.in);
+	 int sum = kb.nextInt();	
+	 int A = kb.nextInt();
+	 
+		for(int i=0;i<10;i++)
+			sum = sum+ A[i];
+			
+		
+	}	
+	
+}
